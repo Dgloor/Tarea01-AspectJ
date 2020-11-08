6dc9c7c7de3ba0b61097edf576a397c3b2990215
eapb99 <emaalpar@espol.edu.ec>
2020-11-08 00:47:07 -0500

    Create the buttons

--- src/app/App.java
+++ src/app/App.java
@@ -1,19 +1,52 @@
 package app;
 
-import javax.swing.JFrame;
+import java.awt.Color;
 
-public class App {
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JToggleButton;
+
+
+
+public class App{
 	
 	public static void main(String[] args) {
-		new App();
-	}
+		
+		JFrame frame = new JFrame();
+		
+		JToggleButton btngreen = new JToggleButton("Green");
+		btngreen.setBounds(150,50,100,40);
+		btngreen.setBackground(Color.LIGHT_GRAY);
+		btngreen.setSize(100,35);
+		Font font = new Font("VERDANA",Font.BOLD,15);
+		btngreen.setFont(font);
+		frame.add(btngreen);
 	
-	public App() {
-		JFrame frame = new JFrame("Colors");
-		frame.setSize(500, 500);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JToggleButton btnred= new JToggleButton("Red");
+		btnred.setBounds(150,150,100,40);
+		btnred.setSize(100,35);
+		btnred.setBackground(Color.LIGHT_GRAY);
+		Font font2 = new Font("VERDANA",Font.BOLD,15);
+		btnred.setFont(font2);
+		 
+		 frame.add( btnred);
+	
+		
+		JToggleButton btnorange= new JToggleButton("Orange");
+		btnorange.setBounds(150, 250, 100, 40);
+		btnorange.setBackground(Color.LIGHT_GRAY);
+		btnorange.setSize(100,35);
+		Font font3 = new Font("VERDANA",Font.BOLD,15);
+		btnorange.setFont(font3);
+		
+		frame.add(btnorange);
+		
+		frame.setSize(400,500);
+		frame.setLayout(null);
 		frame.setVisible(true);
+		
+		
 	}
-
 }
